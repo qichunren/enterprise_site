f213f8c8aae236182d675395127d6ced124947ab
Qichunren <whyruby@gmail.com>
2010-11-09 00:09:03 +0800

    #TODO define class methods for direct get value by key

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -1,2 +1,10 @@
 class SiteSetting < ActiveRecord::Base
+
+  class_eval do
+    self.all.each do |record|
+      define_method("self." + record.setting_key) do
+        record.setting_value
+      end
+    end
+  end
 end
