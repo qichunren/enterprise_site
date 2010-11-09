60a7a27b89f5bd36185964c4df6cb85c1f22749a
qichunren <whyruby@gmail.com>
2010-11-10 00:21:05 +0800

    add login link to application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,44 @@ class ApplicationController < ActionController::Base
       logger.info "#{Time.now.to_s(:db)} apperror: #{exception.inspect}, #{exception.application_backtrace}, params:#{params.inspect}"
       render :text => "#{exception.class}"  and return
     end
+  end      
+  
+  
+  helper_method :current_admin_session, :current_admin
+
+  private
+
+  def current_admin_session
+    return @current_admin_session if defined?(@current_admin_session)
+    @current_admin_session = AdminSession.find
+  end
+
+  # get the current logged admin worker.
+  def current_admin
+    return @current_admin if defined?(@current_admin)
+    @current_admin = current_admin_session && current_admin_session.record
+  end
+
+  # before fileter for reuqire a logged admin worker.
+  def require_admin
+    unless current_admin
+      store_location
+      flash[:error] = "你还没有登录，请先登录！"
+      redirect_to new_admin_session_url
+      return false
+    end
+  end
+  
+  # this will set current admin into Admin model
+  def set_current_admin
+    Admin.current = current_admin
+  end
+
+
+
+  # proxy for acts_as_audit gem
+  def current_user
+    current_admin
   end
 
 end
--- app/controllers/syspanel/base_controller.rb
+++ app/controllers/syspanel/base_controller.rb
@@ -8,41 +8,6 @@ class Syspanel::BaseController < ApplicationController
 
   #before_filter :authorization    # authorization  用户是否有访问这个action的权限
 
-  helper_method :current_admin_session, :current_admin
-
-  private
-
-  def current_admin_session
-    return @current_admin_session if defined?(@current_admin_session)
-    @current_admin_session = AdminSession.find
-  end
-
-  # get the current logged admin worker.
-  def current_admin
-    return @current_admin if defined?(@current_admin)
-    @current_admin = current_admin_session && current_admin_session.record
-  end
-
-  # before fileter for reuqire a logged admin worker.
-  def require_admin
-    unless current_admin
-      store_location
-      flash[:error] = "你还没有登录，请先登录！"
-      redirect_to new_admin_session_url
-      return false
-    end
-  end
   
-  # this will set current admin into Admin model
-  def set_current_admin
-    Admin.current = current_admin
-  end
-
-
-
-  # proxy for acts_as_audit gem
-  def current_user
-    current_admin
-  end
   
 end
