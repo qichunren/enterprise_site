012ca472678ff75095090c53d26b0b143400c3b2
qichunren <whyruby@gmail.com>
2010-11-21 17:47:34 +0800

    better for pages manage

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,7 @@
-class Page < ActiveRecord::Base
+class Page < ActiveRecord::Base                               
+  validates :title, :presence => true
   validates :url_slug, :presence => true, :uniqueness => true 
+  validates :body, :presence => true
   
   scope :published, :conditions => ["is_published=true", true], :order => "url_slug"
 end
