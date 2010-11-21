c0402ef6a5a97dfd823e60468f2cad5206255947
qichunren <whyruby@gmail.com>
2010-11-21 17:57:42 +0800

    better for site setting

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -1,6 +1,9 @@
-class SiteSetting < ActiveRecord::Base 
+class SiteSetting < ActiveRecord::Base
   
+   
+  validates :name, :presence => true
   validates :setting_key, :presence => true, :uniqueness => true
+  validates :setting_value, :presence => true
 
   class_eval do
     self.all.each do |record|
