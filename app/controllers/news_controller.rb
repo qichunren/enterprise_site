44bab9d079c55d9568d29297f6a87f389cd9daff
qichunren <whyruby@gmail.com>
2011-02-07 14:35:42 +0800

    浏览新闻时，阅读次数加1

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -11,9 +11,13 @@ class NewsController < ApplicationController
 
   # GET /news/1
   def show
-    @news = News.find(params[:id])  
-    @news.click_count = @news.click_count + 1
-    @news.save
+    @news = News.find(params[:id])
+    if cookies["news_#{@news.id}_read".to_sym].blank?  
+      @news.click_count = @news.click_count + 1
+      @news.save                                                                     
+      # TODO cookie应该设为一个小时内有效
+      cookies["news_#{@news.id}_read".to_sym] = "read"                                
+    end
 
     respond_to do |format|
       format.html # show.html.erb
