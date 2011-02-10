1a63b44e462ae89f22be7cc6e643cd7fb670c1dc
qichunren <whyruby@gmail.com>
2011-02-10 20:48:13 +0800

    add contact_us info to sidebar in feedback#new page form.

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8 
 class NoticesController < ApplicationController  
   
   # GET /notices
--- app/controllers/syspanel/home_controller.rb
+++ app/controllers/syspanel/home_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8 
 class Syspanel::HomeController < Syspanel::BaseController
   
   def index
