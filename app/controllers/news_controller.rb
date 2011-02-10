bd97caeb7db5af2bafddd550d760bd506c658c45
qichunren <whyruby@gmail.com>
2011-02-10 17:04:27 +0800

    add editor to news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,9 @@
+# encoding: utf-8
 class NewsController < ApplicationController
 
   # GET /news
   def index
-    @news = News.paginate :page => params[:page], :per_page => 10
+    @news = News.order("id DESC").paginate :page => params[:page], :per_page => 10
 
     respond_to do |format|
       format.html # index.html.erb
