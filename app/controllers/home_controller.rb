da52df167f6fc1d3913b3d0395a2d3fdaed99947
Qichunren 曹金华 <whyruby@gmail.com>
2011-06-23 00:23:41 +0800

    refact application nav link style.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 # encoding: utf-8 
 class HomeController < ApplicationController
-  
+
   def index
     # @latest_notices = Notice.published.limit 5      
     # @latest_products = Product.published.limit(SiteSetting.got(:latest_product_size).to_i)
-  end 
-
+  end
 end
